e8102ff0981e73498c8473463f5e60036f629112
tricky93 <ptmwalsh1@gmail.com>
2018-07-13 12:58:11 +0100

    add header component to display school name upon login

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import AuthUserContext from "./AuthUserContext";
+
+class Header extends Component {
+  render() {
+    return this.props.user ? (
+      <div>
+        <h1>{this.props.user.schoolName}</h1>
+      </div>
+    ) : null;
+  }
+}
+
+export default props => {
+  return (
+    <AuthUserContext.Consumer>
+      {user => <Header {...props} user={user} />}
+    </AuthUserContext.Consumer>
+  );
+};
