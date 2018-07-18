9bf85b45048bed6525fe22330b4be8aaa99737d3
hannahbennett27 <37983286+hannahbennett27@users.noreply.github.com>
2018-07-18 09:56:03 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,9 @@ const Home = () => {
         <div className="col-1" />
         <div className="container col-10">
           <h1 className="display-4 text-center text-secondary">
-            An educational RPG to solve the problem of poor financial education
-            in school leavers.
+            An educational role-playing game; introducing school classrooms to
+            financial responsibility and helping to solve the problem of poor
+            financial know-how in school leavers
           </h1>
           <hr className="my-4" />
         </div>
@@ -18,23 +19,23 @@ const Home = () => {
         <div className="row">
           <div className="col-4 text-center">
             <i className="fas fa-chart-line display-3" />
-            <h2 className="mt-2">Data</h2>
+            <h2 className="mt-2">Analysis</h2>
             <p>
-              The game collects analytical data on player choices giving
-              real-time visual feedback to the teacher.
+             Collects data on player choices, giving real-time and
+              retrospective visual feedback to the teacher
             </p>
           </div>
           <div className="col-4 text-center">
             <i className="fas fa-question display-3" />
             <h2 className="mt-2">Realistic</h2>
-            <p>In-game decisions mimic real life</p>
+            <p>In-game circumstances and decision making mimic real life</p>
           </div>
           <div className="col-4 text-center">
             <i className="fas fa-plus display-3" />
             <h2 className="mt-2">Modern</h2>
             <p>
-              Being a progressive web app, the game can be played by children in
-              remote areas
+              A Progressive Web App; playable by children online and offline,
+              even in remote areas
             </p>
           </div>
         </div>
