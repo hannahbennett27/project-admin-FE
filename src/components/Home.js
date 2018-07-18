a6233edd66f6441c0bded1a4ffc462e982ba591f
hannahbennett27 <hannah.bennett27@hotmail.co.uk>
2018-07-18 10:32:14 +0100

    home page to be replaced by previous dev pull

---
+++ src/components/Home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+const Home = () => {
+  return (
+    <div>
+      <h1 className="display-1 text-center mb-5 mt-5">
+        Bacon<span>
+          <i className="fas fa-piggy-bank display-4" id="pig" />
+        </span>
+      </h1>
+      <div className="row">
+        <div className="col-1" />
+        <div className="container col-10">
+          <h1 className="display-4 text-center text-secondary">
+            An educational role-playing game; introducing school classrooms to
+            financial responsibility and helping to solve the problem of poor
+            financial know-how in school leavers
+          </h1>
+          <hr className="my-4" />
+        </div>
+        <div className="col-1" />
+      </div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-4 text-center">
+            <i className="fas fa-chart-line display-3" />
+            <h2 className="mt-2">Analysis</h2>
+            <p>
+              Collects data on player choices, giving real-time and
+              retrospective visual feedback to the teacher
+            </p>
+          </div>
+          <div className="col-4 text-center">
+            <i className="fas fa-question display-3" />
+            <h2 className="mt-2">Realistic</h2>
+            <p>Real life in-game circumstances and decision making</p>
+          </div>
+          <div className="col-4 text-center">
+            <i className="fas fa-plus display-3" />
+            <h2 className="mt-2">Modern</h2>
+            <p>
+              A Progressive Web App; playable by children online and offline,
+              even in remote areas
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
